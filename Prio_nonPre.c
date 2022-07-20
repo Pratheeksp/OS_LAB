60c36a1ac8289821d38cf0d53fcabbcd8437f198
Pratheek S P <96907974+Pratheeksp@users.noreply.github.com>
2022-07-20 22:29:29 +0530

    Update Prio_nonPre.c

--- Prio_nonPre.c
+++ Prio_nonPre.c
@@ -28,7 +28,7 @@ void sortPT(p *a,int n){
 
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
-            if((a[i].pt > a[j].pt) && (a[j].at < burst)){
+            if((a[i].pt > a[j].pt) && (a[j].at <= burst)){
                 temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
